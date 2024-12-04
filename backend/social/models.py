7199c7b33e7ca9a006224e2cece0ed94b7e07fd2
Jake Richards <contact@jakerichards.info>
2024-12-03 16:51:30 -0800

    add post and textpost model types

--- backend/social/models.py
+++ backend/social/models.py
@@ -11,3 +11,21 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Post(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.account.user.username} - {self.created_at}"
+
+
+class TextPost(models.Model):
+    post = models.OneToOneField(
+        Post, on_delete=models.CASCADE, related_name="text_post"
+    )
+    content = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.content
