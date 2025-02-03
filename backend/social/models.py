2bfa821001a8134f1f5b26ac3cd822a5ab1f6895
Jake Richards <contact@jakerichards.info>
2025-02-03 14:07:50 -0800

    refactor(API): :recycle: adjust upload path to users own directory

--- backend/social/models.py
+++ backend/social/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from datetime import datetime
 
 User = get_user_model()
 
@@ -41,11 +42,16 @@ class MarkdownPost(models.Model):
         return self.content
 
 
+def user_image_path(self, filename):
+    username = self.post.account.user.username
+    return f'users/{username}/images/{datetime.now().strftime("%Y/%m/%d")}/{filename}'
+
+
 class ImagePost(models.Model):
     post = models.OneToOneField(
         Post, on_delete=models.CASCADE, related_name="image_post"
     )
-    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+    image = models.ImageField(upload_to=user_image_path)
     caption = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
