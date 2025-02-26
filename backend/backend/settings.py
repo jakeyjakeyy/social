73c01564d9ef220889d808842feebe33e25c23b1
Jake Richards <contact@jakerichards.info>
2025-02-26 00:25:39 -0800

    fix: extend refresh token lifetime to 30 days; update color theme and make sure it works in both scenarios

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
 MEDIA_URL = "/media/"
