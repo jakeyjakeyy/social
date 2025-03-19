fe67799c4a91edb7e40a52a895b82a698b5ea9ce
Jake Richards <contact@jakerichards.info>
2025-03-19 14:24:32 -0700

    refactor: remove unused font imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,6 @@ import Nav from "@/components/Nav.vue";
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap");
 @import "@/assets/styles/design-system.css";
 
 #app {
