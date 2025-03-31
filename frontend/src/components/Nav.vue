4a48445e367f9872019e508ab576ebb4ec21f763
Jake Richards <contact@jakerichards.info>
2025-03-31 12:45:48 -0700

    fix(Nav): resize event handling

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -2,7 +2,7 @@
 import Auth from "./Auth.vue";
 import AddPost from "./AddPost.vue";
 import ThemeSelector from "./ThemeSelector.vue";
-import { onMounted, ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import { deleteTokens, checkToken } from "@/utils/RefreshToken";
 import Notifications from "./Notifications.vue";
@@ -20,23 +20,31 @@ const theme = ref("dark");
 onMounted(() => {
   window.addEventListener("resize", () => {
     screen.value = window.innerWidth;
+    checkNavIsMobile();
   });
 
-  if (screen.value < 768) {
-    navIsMobile.value = true;
-  }
+  checkNavIsMobile();
 
   theme.value = localStorage.getItem("theme") || "dark";
 });
+
+onUnmounted(() => {
+  window.removeEventListener("resize", checkNavIsMobile);
+});
+
 const navItems = [
   { name: "Home", path: "/" },
   { name: "Profile", path: "/profile" },
   { name: loggedIn.value ? "Logout" : "Login", path: "/auth" },
 ];
 
-const toggleNavItems = () => {
-  showNavItems.value = !showNavItems.value;
-};
+function checkNavIsMobile() {
+  if (screen.value < 768) {
+    navIsMobile.value = true;
+  } else {
+    navIsMobile.value = false;
+  }
+}
 
 const navigateTo = (path: string) => {
   if (path === "/auth") {
